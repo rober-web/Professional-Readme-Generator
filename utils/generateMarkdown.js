ac1da5d915af9dc7ece918b9ac439eb4f7024cb3
rober-web <robp.medeiros@gmail.com>
2024-02-19 00:08:18 +0000

    added comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,43 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+
+  //Variable to contain the content of the readme file
   let theContent = "";
+
+   /* Added if statement to print only the content user decides to add to their README file
+   and ignore the ones they skipp on the prompt */
 
   if (data.projectTitle) {
     theContent += `# ${data.projectTitle}\n`;
   }
 
+  //The table of contents function will only be displayed if any of the content is filled by user
   const tableOfContents = (data) => {
+
+    // Table of contents variable will start empty
     let tocContent = "";
-    let tocContentTitle = '## Table of Contents';;
+
+     /* Table of contents title variable will start  with its title by default
+     It only will be empty case tocContent variable remains empty */
+    let tocContentTitle = '## Table of Contents';
+
+    //Case any data is true, starts the condition scope
     if (data) {
+
+      //If no content is added
       if(tocContent === "") {
+
+        //then table of contents does not display the title
         tocContentTitle = '';
       }
       
-      tocContent += `
+      /* Add data to tocContent if true
+      and print the table of contents title. 
+      The contents under the TBC will be displayed with anchors.*/
 
-  ${tocContentTitle}\n
-  `;
+      tocContent += `
+      ${tocContentTitle}\n
+`;
       if (data.installation) {
         tocContent += `- [Installation](#installation)\n`;
       }
@@ -36,7 +56,7 @@ function generateMarkdown(data) {
       if (data.questions) {
         tocContent += `- [Questions](#questions)\n`;
       }
-      // Add more sections as needed
+
     }
 
     return tocContent;
@@ -46,13 +66,13 @@ function generateMarkdown(data) {
     theContent += `## Description\n${data.description}\n`;
   }
 
-  // Add Table of Contents using the separate function
+  /* Add Table of Contents using the separate function at the right position
+     just after the description section */
   theContent += tableOfContents(data);
 
   if (data.installation) {
     theContent += `## Installation\n${data.installation}\n`;
   }
-
 
   if (data.usage) {
     theContent += `## Usage\n${data.usage}\n`;
